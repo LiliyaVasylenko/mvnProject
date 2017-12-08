8e7fcf0e3f9aa2949a3a00cadc9a2b5988a93d48
Liliia.Vasylenko <Liliia.Vasylenko@infopulse.com>
2017-12-08 14:56:17 +0200

    Add test suite

--- src/test/java/lesson_5/TestSuite.java
+++ src/test/java/lesson_5/TestSuite.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({LoginTest.class, SearchTest.class})
+@Suite.SuiteClasses({LoginTest.class, lesson_5.SearchTest.class})
 public class TestSuite {
 }
